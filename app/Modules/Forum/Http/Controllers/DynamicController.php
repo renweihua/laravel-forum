d0d740e6c67fcba6a974efb4a210b149a876f29b
小丑路人 <2278757482@qq.com>
2023-06-27 17:15:07 +0800

    optimize 动态不存在或已删除

--- app/Modules/Forum/Http/Controllers/DynamicController.php
+++ app/Modules/Forum/Http/Controllers/DynamicController.php
@@ -9,6 +9,9 @@ class DynamicController extends ForumController
     public function show($id)
     {
         $dynamic = Dynamic::with(['user', 'userInfo'])->find($id);
+        if (empty($dynamic)){
+            abort(404, '动态不存在或已删除！');
+        }
         // 会员的空间数量
         $dynamic->user->dynamic_count = Dynamic::getDynamicsTotalByUser($dynamic->user_id);
         return view('forum::detail', compact('dynamic'));
