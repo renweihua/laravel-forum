f834c3191ea26c336acf73e02816eadbfd09136d
小丑路人 <2278757482@qq.com>
2023-06-29 16:12:40 +0800

    评论之后，给动态创建人发送Job通知消息；如果评论本身就是动态创建人，则无需发送。

--- app/Modules/Comment/Http/Controllers/CommentsController.php
+++ app/Modules/Comment/Http/Controllers/CommentsController.php
@@ -4,6 +4,7 @@ namespace App\Modules\Comment\Http\Controllers;
 
 use App\Modules\Comment\Http\Requests\CommentRequest;
 use App\Modules\Comment\Entities\DynamicComment;
+use App\Modules\Comment\Jobs\DynamicCommentNotifyJob;
 use Illuminate\Support\Facades\Auth;
 
 class CommentsController extends CommentModuleController
@@ -20,6 +21,9 @@ class CommentsController extends CommentModuleController
         $comment->comment_content = $request->comment_content;
         $comment->dynamic_id = $request->dynamic_id;
         $comment->save();
+
+        // 通知动态作者有新的评论/回复
+        DynamicCommentNotifyJob::dispatch($comment);
 
         return redirect()->to(route('dynamic.show', [$comment->dynamic_id]))->with('success', '评论创建成功！');
     }
---
+++ app/Modules/Comment/Jobs/DynamicCommentNotifyJob.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Modules\Comment\Jobs;
+
+use App\Modules\Forum\Entities\Notify;
+use App\Modules\Comment\Entities\DynamicComment;
+use App\Modules\User\Entities\UserInfo;
+use Illuminate\Bus\Queueable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Symfony\Component\HttpFoundation\Exception\BadRequestException;
+
+class DynamicCommentNotifyJob implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    public $dynamicComment;
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct(DynamicComment $dynamicComment)
+    {
+        // 注入回复实体，方便 toDatabase 方法中的使用
+        $this->dynamicComment = $dynamicComment;
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        $comment = $this->dynamicComment;
+        $dynamic = $comment->dynamic;
+        // 如果评论人本身就是动态发布人，则不发送通知消息
+        if ($dynamic->user_id == $comment->user_id){
+            return;
+        }
+        $notify = Notify::insert([
+            'notify_type'  => Notify::NOTIFY_TYPE['INTERACT_MSG'],
+            'user_id'      => $dynamic->user_id,
+            'target_id'    => $dynamic->dynamic_id,
+            'target_type'  => Notify::TARGET_TYPE['DYNAMIC'],
+            'sender_id'    => $comment->user_id,
+            'sender_type'  => Notify::SYSTEM_SENDER,
+            'dynamic_type' => $comment->reply_id == 0 ? Notify::DYNAMIC_TARGET_TYPE['COMMENT'] : Notify::DYNAMIC_TARGET_TYPE['REPLY_COMMENT'],
+            'extend_id'    => $comment->comment_id,
+        ]);
+        if (!$notify) {
+            throw new BadRequestException('互动`通知`消息录入失败！');
+        }
+        // 作者的通知数量累加1
+        UserInfo::find($notify->user_id)->increment('notification_count');
+    }
+}
