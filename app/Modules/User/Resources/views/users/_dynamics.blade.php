e1c367a32f0953b4008939d05b75ebc9226bb84e
小丑路人 <2278757482@qq.com>
2023-07-18 15:47:13 +0800

    会员主页的动态列表，展示 点赞、收藏、评论统计

--- app/Modules/User/Resources/views/users/_dynamics.blade.php
+++ app/Modules/User/Resources/views/users/_dynamics.blade.php
@@ -6,17 +6,39 @@
                 <a href="{{ $dynamic->link() }}">
                     {{ $dynamic->dynamic_title }}
                 </a>
-                <span class="meta float-right text-secondary">
-          {{ $dynamic->cache_extends['comments_count'] }} 回复
-          <span> ⋅ </span>
-          {{ $dynamic->time_formatting }}
-        </span>
+                <small class="meta float-right text-secondary">
+                    <i class="fa fa-eye"></i>
+                    <span title="最后活跃于：{{ date('Y-m-d H:i', $dynamic->updated_time) }}">
+                            {{ $dynamic->cache_extends['reads_num'] }}</span>
+
+                    <span> • </span>
+                    <small id="praise" class="cursor-pointer" @click="praise({{$dynamic->dynamic_id}})">
+                        <i class="fa {{ $dynamic->is_praise ? 'fa-thumbs-up' : 'fa-thumbs-o-up' }} faa-bounce animated"></i>
+                        <span>{{ $dynamic->cache_extends['praises_count'] }}</span>
+                    </small>
+
+                    <span> • </span>
+                    <small id="collection" class="cursor-pointer" @click="collection({{$dynamic->dynamic_id}})">
+                        <i class="fa {{ $dynamic->is_collection ? 'fa-heartbeat' : 'fa-heart-o' }} faa-pulse animated-hover faa-slow"></i>
+                        <span>{{ $dynamic->cache_extends['collections_count'] }}</span>
+                    </small>
+
+                    <span> • </span>
+                    <i class="fa fa-comments-o fa-lg"></i>
+                    <span>{{ $dynamic->cache_extends['comments_count'] }}</span>
+
+                    <span> ⋅ </span>
+                    {{ $dynamic->time_formatting }}
+
+                    <span> ⋅ </span>
+                    {{ $dynamic->last_time_formatting }}
+                </small>
             </li>
         @endforeach
     </ul>
 
 @else
-    <div class="empty-block">暂无数据 ~_~ </div>
+    <div class="empty-block text-center">暂无数据 ~_~ </div>
 @endif
 
 {{-- 分页 --}}
