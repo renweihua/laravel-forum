11b428a91985635819702c3b8f7d2e626761b8c5
小丑路人 <2278757482@qq.com>
2023-07-26 11:07:39 +0800

    中间件RecordLastActivedTime：检测token失效，则重选生成并存储

--- app/Modules/User/Http/Middleware/RecordLastActivedTime.php
+++ app/Modules/User/Http/Middleware/RecordLastActivedTime.php
@@ -5,6 +5,7 @@ namespace App\Modules\User\Http\Middleware;
 use Closure;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Tymon\JWTAuth\Facades\JWTAuth;
 
 class RecordLastActivedTime
 {
@@ -19,8 +20,16 @@ class RecordLastActivedTime
     {
         // 如果是登录用户的话
         if (Auth::check()) {
+            $user = Auth::user();
             // 记录最后登录时间
-            Auth::user()->recordLastActivedAt();
+            $user->recordLastActivedAt();
+
+            // 如果token失效了，则重新生成并存储
+            if (!session()->has('login_token')){
+                $token = JWTAuth::fromUser($user);
+                // 存储登录会员的token
+                session()->put('login_token', $token);
+            }
         }
 
         return $next($request);
