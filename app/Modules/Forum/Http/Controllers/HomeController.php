5eef2666b24ffad4eb8a846f48af8bfb15494bcb
小丑路人 <2278757482@qq.com>
2023-07-26 17:06:50 +0800

    optimize 首页动态：是否点赞与收藏

--- app/Modules/Forum/Http/Controllers/HomeController.php
+++ app/Modules/Forum/Http/Controllers/HomeController.php
@@ -14,18 +14,35 @@ class HomeController extends ForumController
         $tab = $request->input('tab', 'default');
         $search = $request->input('q', '');
 
-        $dynamics = Dynamic::public()
-            ->filter($request->all())
-            ->with(['topic', 'userInfo'])
-            ->paginate(15);
-
         $login_user = Auth::user();
+        $login_user_id = $login_user->user_id ?? 0;
         if ($login_user){
             // 是否已签到
             $login_user->userInfo->is_sign = false;
             if ($login_user->userInfo->last_sign_time > 0 && date('Y-m-d') == date('Y-m-d', $login_user->userInfo->last_sign_time)){
                 $login_user->userInfo->is_sign = true;
             }
+        }
+
+        $dynamics = Dynamic::public()
+            ->filter($request->all())
+            ->with([
+                'topic',
+                'userInfo',
+                'isPraise' => function($query) use ($login_user_id) {
+                    $query->where('user_id', $login_user_id);
+                },
+                'isCollection' => function($query) use ($login_user_id) {
+                    $query->where('user_id', $login_user_id);
+                },
+            ])
+            ->paginate(15);
+
+        foreach ($dynamics as $dynamic){
+            // 是否已赞
+            $dynamic->is_praise = $login_user_id == 0 ? false : ($dynamic->isPraise ? true : false);
+            // 是否已收藏
+            $dynamic->is_collection = $login_user_id == 0 ? false : ($dynamic->isCollection ? true : false);
         }
 
         return view('forum::index', compact('dynamics', 'tab', 'search', 'login_user'));
